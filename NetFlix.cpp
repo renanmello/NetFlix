5bba08dc35287de6176bd87368e808368364ac40
renan mello <mellorenan19@gmail.com>
2017-01-16 17:24:34 -0300

    Modificações da classe

--- NetFlix.cpp
+++ NetFlix.cpp
@@ -1,4 +1,17 @@
 #include "NetFlix.h"
+#include "stdafx.h"
+#include "iostream"
+using std::cout;
+
+int main()
+{
+	cout << "Ola mundo";
+	system("pause");
+
+	
+	return 0;
+
+}
 
 NetFlix::NetFlix()
 {
@@ -7,4 +20,14 @@ NetFlix::NetFlix()
 NetFlix::~NetFlix()
 {
 }
+
+void NetFlix::classificaFilme()
+{
+
+}
+
+void NetFlix::exibirDados()
+{
+}
+
 
--- NetFlix.h
+++ NetFlix.h
@@ -1,11 +1,19 @@
 #ifndef NETFLIX_H
 #define NETFLIX_H
+#include "iostream"
+using namespace std;
 
 class NetFlix
 {
+    private:
+	int senha;
+	string nome;
+
 public:
     NetFlix();
     ~NetFlix();
+    void classificaFilme();
+	void exibirDados();
 
 };
 
