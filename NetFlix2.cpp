02f371ef10b62b2aca6b0557ffd21556d1e5ebb5
renan <mellorenan19@gmail.com>
2017-02-19 20:06:45 -0300

    mudança nas classes

---
+++ NetFlix2.cpp
@@ -0,0 +1,91 @@
+#include "NetFlix2.h"
+#include <iostream>
+#include <string>
+using std::string;
+using std::cout;
+using std::cin;
+
+NetFlix2::NetFlix2()
+{
+    senha="default";
+    nome="default";
+}
+
+NetFlix2::~NetFlix2()
+{
+    delete [] listaDePessoas;
+    delete [] listaDeFilmes;
+}
+
+NetFlix2::NetFlix2(const string &senha, const string &nome){
+    this->senha=senha;
+    this->nome=nome;
+    }
+
+const NetFlix2 &operator=(const NetFlix2 &net){
+        this->senha=net.senha;
+        this->nome=net.nome;
+        
+        }
+        
+        bool NetFlix2::operator==(const NetFlix2 &net){
+             if(net.senha==0){
+                 return false;
+                 }else{
+                     return true;
+                     }
+             }
+             
+             NetFlix2::NetFlix2(const NetFlix2 &net){
+                 senha=net.senha;
+                 nome=net.nome;
+                 
+                 }
+                 
+     ostream &operator<<(ostream &output, const NetFlix2 &net){
+     output<<"Nome:"<<net.nome<<" senha:"<<net.senha;
+     return output;
+     }
+
+ void NetFlix2::NetFlix2(const int nUsers, const int nFilmes){
+     alocarUsuario(nUsers);
+     alocarFilme(nFilmes);
+     }
+     
+    void NetFlix2::alocarUsuario(const int nUsers){
+         if(nUsers>0){
+        listaDePessoas=new string[nUsers];
+        quatPessoas=nUsers;
+        cout<<"números de pessoas para cadastro";
+        }else{
+            cout<<"sem pessoas cadastradas";
+            }
+         }
+         
+         void NetFlix2::alocarFilme(const int nFilmes){
+         if(nFilmes>0){
+        listaDeFilmes=new string[nFilmes];
+        quatFilmes=nFilmes;
+        cout<<"números de filmes para cadastro";
+        }else{
+            cout<<"sem filmes no cadastro";
+            }
+         }
+         
+         void NetFlix2::cadastrarFilme(){
+             
+             for(int i=0,i<listaDeFilmes.lenght,i++){
+                 
+                 cout<<"digite o nome do "<<i<<"Filme";
+                 cin>>nomeFilme;
+                 
+                 cout<<"Digite o tipo do filme";
+                 cin>>tipoFilme;
+                 
+                 }
+                 
+                 cout<<"Fim dos cadastros";
+             
+             }
+     
+     
---
+++ NetFlix2.h
@@ -0,0 +1,41 @@
+#ifndef NETFLIX2_H
+#define NETFLIX2_H
+#include <iostream>
+#include <string>
+
+class NetFlix2 : public Streaming
+{
+    friend ostream &operator<<(ostream &,const NetFlix2 &);
+public:
+    NetFlix2();
+    ~NetFlix2();
+
+    NetFlix2(const string &, const string &);
+    NetFlix2(const NetFlix2 &);
+    const NetFlix2 &operator=(const NetFlix2 &);
+    bool operator==(const NetFlix2 &);
+    virtual void classificarFilmes();
+    virtual void exibirDados() const;
+    
+protected:
+
+    string senha;
+	string nomeFilme;
+    string tipoFilme;
+    static string regiao;
+    string *listaDeFilmes;
+    bool like;
+    
+private:
+	
+    void alocarUsuario(const int);
+    void alocarFilme(const int);
+    void cadastrarFilme();
+    void NetFlix2(const int, const int);
+    int quatPessoas;
+    int quatFilmes;
+    string *listaDePessoas;
+    string *listaDeFilmes;
+};
+
+#endif // NETFLIX2_H
